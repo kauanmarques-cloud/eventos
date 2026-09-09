4197845ca61956f355a22c59c76d7028633f126e
kauanmarques-cloud <kauan.marques@escolar.ifrn.edu.br>
2026-09-09 11:10:41 -0300

    criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventoController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventoController.java
@@ -1,31 +1,43 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Eventos;
 import ifrn.pi.eventos.repositories.EventosRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventoController {
 
 	@Autowired
 	private EventosRepository er;
 	
-	@RequestMapping("/eventos/form")
+	@GetMapping("/form")
 	public String form() {
 		return "eventos/formEvento" ; 
 	}
 	
-	@RequestMapping( path = "/eventos" , method = RequestMethod.POST)
+	@RequestMapping
 	public String adicionar(Eventos evento) {
 		
 			System.out.println(evento);
 			er.save(evento);	
 				
 		return "eventos/evento-adicionado";
+	}
+	@GetMapping
+	public ModelAndView listar() {
+		List<Eventos> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 	
 }
